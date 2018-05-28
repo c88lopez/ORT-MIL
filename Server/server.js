38b4179f1386245300dd0cd1328ee02675e2a562
Cristian Lopez <c88lopez@gmail.com>
2018-05-27 23:43:22 -0300

    detectando e informando movimiento (beta)

--- Server/server.js
+++ Server/server.js
@@ -1,78 +1,107 @@
-var express = require('express');
-//var app = require('express')();
-var app = express();
-var bodyParser = require('body-parser');
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var path = require('path');
-var os = require('os');
-var fs = require('fs');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const path = require('path');
+const os = require('os');
+// const fs = require('fs');
 
 app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-var interfaces = os.networkInterfaces();
-var addresses = [];
+const interfaces = os.networkInterfaces();
+const addresses = [];
 for (var k in interfaces) {
     for (var k2 in interfaces[k]) {
-        var address = interfaces[k][k2];
-        if (address.family === 'IPv4' && !address.internal) {
-            addresses.push(address.address);
+        if (interfaces[k][k2].family === 'IPv4' && !interfaces[k][k2].internal) {
+            addresses.push(interfaces[k][k2].address);
         }
     }
 }
 
-var accelData;
-var accelX;
-var accelY;
-var accelZ;
-var temp;
-var gyroX;
-var gyroY;
-var gyroZ;
-var location;
-var macAddress;
+const devices = {};
+const dataStoredPerDevice = 2;
 
 server.listen(3000, process.argv[2], function() {
-	var host = server.address().address
-    var port = server.address().port
-    
-	console.log("Server listening on %s:%s...", host, port);
+    console.log(
+        "Server listening on %s:%s...", 
+        server.address().address, 
+        server.address().port
+    );
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/location', function(req, res) {
-	location = req.body
-	io.emit('location', location);	
-	console.log(req.body.city)
+    location = req.body
+    io.emit('location', location);	
+    console.log(req.body.city)
 });
 
 app.post('/accel', function(req, res) {
-  accelData = req.body
+    const currentTime = Date.now();
+    const macAddress = req.body.data.macAddress;
 
-  io.emit('accelData', accelData);
+    const data = {
+        accel: {
+            x: req.body.data.accel.x,
+            y: req.body.data.accel.y,
+            z: req.body.data.accel.z,
+        },
+        gyro: {
+            x: req.body.data.gyro.x,
+            y: req.body.data.gyro.y,
+            z: req.body.data.gyro.z,
+        },
+    }
 
-  accelX = req.body.data.accel.x
-  accelY = req.body.data.accel.y
-  accelZ = req.body.data.accel.z
-  gyroX = req.body.data.gyro.x
-  gyroY = req.body.data.gyro.y
-  gyroZ = req.body.data.gyro.z
-    
-  macAddress = req.body.data.macAddress.split(':').join('_');
+    if (devices.hasOwnProperty(macAddress)) {
+        devices[macAddress].unshift(data);
+        if (devices[macAddress].length > dataStoredPerDevice) {
+            devices[macAddress].pop();
+            req.body.data.isMoving = 
+            isMoving(devices[macAddress], 'x') || isMoving(devices[macAddress], 'y') ||isMoving(devices[macAddress], 'z');
+        }
+    } else {
+        devices[macAddress] = [data];
+    }
 
-  fs.appendFileSync(`./${macAddress}.csv`, `${accelX},${accelY},${accelZ},${gyroX},${gyroY},${gyroZ}\n`)
+    io.emit('accelData', req.body);
+
+    // fs.appendFileSync(`./${macAddress}.csv`, `${accelX},${accelY},${accelZ},${gyroX},${gyroY},${gyroZ}\n`)
 });
 
 io.on('connection', function (socket) {
-	socket.on('location', function (data) {
-		console.log(data);
-	});
-	//socket.on('accelData', function (data) {
-		//console.log(data);
-	//});
+    socket.on('location', function (data) {
+        console.log(data);
+    });
+
+    //socket.on('accelData', function (data) {
+        //console.log(data);
+    //});
 });
+
+function isMoving(data, axis) {
+    let accelMax;
+    let accelMin;
+
+    data.forEach(element => {
+        if (typeof accelMax == 'undefined') {
+            accelMax = accelMin = element.accel[axis]
+        } else {
+            if (element.accel[axis] > accelMax) {
+                accelMax = element.accel[axis]
+            }
+
+            if (element.accel[axis] < accelMin) {
+                accelMin = element.accel[axis]
+            }
+        }
+    });
+
+    return (accelMax - accelMin) > 1000
+}
